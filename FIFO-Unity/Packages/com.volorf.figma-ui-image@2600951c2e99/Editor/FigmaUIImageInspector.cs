e4ace104975d0f42a71ed7d6e3c2cb40d5fda09a
soyaoh <139798714+soyaoh@users.noreply.github.com>
2026-04-06 20:36:49 -0700

    unity game update

---
+++ FIFO-Unity/Packages/com.volorf.figma-ui-image@2600951c2e99/Editor/FigmaUIImageInspector.cs
@@ -0,0 +1,116 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Volorf.FigmaUIImage
+{
+    #if UNITY_EDITOR
+    
+        [CustomEditor(typeof(FigmaUIImage))]
+        public class FigmaUiImageInspector : Editor
+        { 
+            VisualTreeAsset _ui;
+            FigmaUIImage _figmaUiImage;
+            VisualElement _previewContainer;
+            VisualElement _root;
+            Label _imageDescription;
+
+            public override VisualElement CreateInspectorGUI()
+            {
+                _ui = Resources.Load<VisualTreeAsset>("FigmaUiImageInspector");
+                _root = new VisualElement();
+                _figmaUiImage = target as FigmaUIImage;
+                _ui.CloneTree(_root);
+                
+                // Get references
+                _previewContainer = _root.Q<VisualElement>("Container");
+                _imageDescription = _root.Q<Label>("imageDescription");
+                Button updateButton = _root.Q<Button>("UpdateButton");
+                Button saveAsAssetButton = _root.Q<Button>("SaveAsAssetButton");
+                
+                // Subscriptions
+                updateButton.RegisterCallback<ClickEvent>(UpdateFigmaImage);
+                saveAsAssetButton.RegisterCallback<ClickEvent>(SaveAsAsset);
+                
+                _figmaUiImage.OnUiImageUpdated.AddListener(UpdatePreview);
+                _figmaUiImage.OnUploadingFailed.AddListener(SetDefault);
+                
+                if (_figmaUiImage.GetRawImage() != null)
+                {
+                    UpdatePreview(new FigmaUIImageData(_figmaUiImage.GetRawImage().texture, _figmaUiImage.GetScale(), FigmaUIImage.GetCurrentDateTime()));
+                }
+
+                SetDisplayStyleForExplanation();
+                
+                return _root;
+            }
+
+            void SetDisplayStyleForExplanation()
+            {
+                // _explanation.style.display = _figmaUiImage.GetFigmaUIData() != null ? new StyleEnum<DisplayStyle>(DisplayStyle.None) : new StyleEnum<DisplayStyle>(DisplayStyle.Flex);
+            }
+
+            void UpdateFigmaImage(ClickEvent ev)
+            {
+                SetDisplayStyleForExplanation();
+                StartPreloading();
+
+                if (String.IsNullOrEmpty(_figmaUiImage.GetFigmaLink()))
+                {
+                    SetDefault();
+                    Debug.LogError("Add a Figma Link.");
+                    return;
+                }
+                
+                if (String.IsNullOrEmpty(_figmaUiImage.GetToken()))
+                {
+                    SetDefault();
+                    Debug.LogError("Add a Figma Token.");
+                    return;
+                }
+                
+                _figmaUiImage.UpdateFigmaImage();
+            }
+
+            void SaveAsAsset(ClickEvent ev)
+            {
+                _figmaUiImage.SaveAsAsset();
+                Debug.Log("Saving Figma Image");
+            }
+
+            void StartPreloading()
+            {
+                _imageDescription.text = "Loading...";
+                _previewContainer.style.backgroundImage = new StyleBackground(_figmaUiImage.GetLoadingTexture() as Texture2D);
+            }
+
+            void StopPreloading()
+            {
+                
+            }
+
+            void SetDefault()
+            {
+                _imageDescription.text = "No Figma Image";
+                _previewContainer.style.backgroundImage = new StyleBackground(_figmaUiImage.GetDefaultTexture() as Texture2D);
+            }
+
+            void UpdatePreview(FigmaUIImageData fd)
+            {
+                _previewContainer.style.backgroundImage = new StyleBackground(fd.GetTexture() as Texture2D);
+
+                string descText = $"Original size is {fd.GetOriginalWidth()}x{fd.GetOriginalHeight()}. Ratio is {fd.GetRatio().ToString("0.###")}";
+                _imageDescription.text = descText;
+                _previewContainer.style.width = fd.GetOriginalWidth();
+                _previewContainer.style.height = fd.GetOriginalHeight();
+                
+                StopPreloading();
+            }
+        }
+    
+    #endif
+}
+
