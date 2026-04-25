a90152e88078fa0ffc86fa37d087df38a3a74227
agrechu <aliciagrechu@gmail.com>
2026-04-24 20:07:06 -0700

    updated UI to make all the icons appear in the right place and look pretty like the figma mockup for streak manager

--- FIFO-Unity/Assets/Scripts/StreakDaySystem.cs
+++ FIFO-Unity/Assets/Scripts/StreakDaySystem.cs
@@ -17,6 +17,10 @@ public class StreakDayUI : MonoBehaviour
     public Sprite checkIcon;
     public Sprite xIcon;
     public Sprite exclamationIcon;
+    
+    [Header("Icon Sizes")]
+    public Vector2 normalIconSize = new Vector2(80, 80);
+    public Vector2 exclamationIconSize = new Vector2(35, 70);
 
     public void SetDay(string dayLabel, StreakManager.Status state)
     {
@@ -28,12 +32,16 @@ public class StreakDayUI : MonoBehaviour
         {
             case StreakManager.Status.Successful:
                 circle.sprite = darkPinkCircle;
+                icon.enabled = true;
                 icon.sprite = checkIcon;
+                icon.rectTransform.sizeDelta = normalIconSize;
                 break;
 
             case StreakManager.Status.Unsuccessful:
                 circle.sprite = darkPinkCircle;
+                icon.enabled = true;
                 icon.sprite = xIcon;
+                icon.rectTransform.sizeDelta = normalIconSize;
                 break;
 
             case StreakManager.Status.Today:
@@ -48,7 +56,9 @@ public class StreakDayUI : MonoBehaviour
 
             case StreakManager.Status.Expires:
                 circle.sprite = lightPinkCircle;
+                icon.enabled = true;
                 icon.sprite = exclamationIcon;
+                icon.rectTransform.sizeDelta = exclamationIconSize;
                 break;
         }
     }
