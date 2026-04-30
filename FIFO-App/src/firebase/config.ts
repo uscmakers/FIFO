84e64ecc646bc3bdd6f980d3df40fcea8c6b1e1e
agrechu <aliciagrechu@gmail.com>
2026-04-29 21:12:16 -0700

    Squashed commit of the following:
    commit 1e4ae9f11781bb7fcdfdd0a7bcbed167fafd031c
    Merge: 44f57d2a e2a17048
    Author: Hannah Lee <hlee4786@usc.edu>
    Date:   Mon Apr 27 22:37:48 2026 -0700
    
        Merge pull request #1 from uscmakers/hannah-app
    
        hannah-app to main
    
    commit e2a17048837dd4e5479080d72f3493a59d56e5ac
    Merge: 17bffac5 44f57d2a
    Author: Hannah Lee <hlee4786@usc.edu>
    Date:   Mon Apr 27 22:37:37 2026 -0700
    
        Merge branch 'main' into hannah-app
    
    commit 17bffac5b01577d71d942c83bf02ce08361e44e3
    Author: hlee4786 <hlee4786@usc.edu>
    Date:   Mon Apr 27 22:32:00 2026 -0700
    
        minor fixes
    
    commit f6f8416669ce87ddb0d216c8309af1a65cd3ee2c
    Author: hlee4786 <hlee4786@usc.edu>
    Date:   Mon Apr 27 12:36:46 2026 -0700
    
        feat: front camera scanning + folder clean up
    
    commit d5f8b7518684a6bf24d7fcb8e219b26a345d7326
    Author: hlee4786 <hlee4786@usc.edu>
    Date:   Wed Apr 15 21:53:38 2026 -0700
    
        feat: look-up function + connecting to ngrok server
    
    commit d574fada2876bf0e1a22d6a90bdf7987af200023
    Author: hlee4786 <hlee4786@usc.edu>
    Date:   Mon Apr 13 22:34:01 2026 -0700
    
        added app icon
    
    commit 38530240b714d2a3a84cc99dae20a44924a5687d
    Author: hlee4786 <hlee4786@usc.edu>
    Date:   Wed Apr 8 23:27:33 2026 -0700
    
        feat: product images + fixing barcode scanning
    
    commit 63cb1a1fef321e3bd8dda00a812af2c867e64e04
    Author: hlee4786 <hlee4786@usc.edu>
    Date:   Mon Apr 6 23:20:47 2026 -0700
    
        feat: updated UI + communication with ML system using ngrok
    
    commit 44f57d2ad73efbd99b04396948fed9ced5e9d04f
    Author: RakshetaK <15raksheta.k@gmail.com>
    Date:   Mon Apr 6 20:53:33 2026 -0700
    
        updated scripts with stitching frames into panorama and ML + pulling video integrated
    
    commit c7c0c7711c4617c4d4b8e2d5bd0d34f86772f3b6
    Author: soyaoh <139798714+soyaoh@users.noreply.github.com>
    Date:   Mon Apr 6 20:36:49 2026 -0700
    
        unity game update
    
    commit 5f12a474f5e5385d9e889c8391d2061adcba6827
    Author: hlee4786 <hlee4786@usc.edu>
    Date:   Wed Mar 25 21:50:19 2026 -0700
    
        deleted .env
    
    commit d04681b337d2165d41d0adc50966b502d6d01327
    Author: hlee4786 <hlee4786@usc.edu>
    Date:   Mon Mar 23 22:05:24 2026 -0700
    
        feat: recipe generator
    
    commit a76b836e72a559c95d1eb1ec00b10b008ee43c1a
    Author: hlee4786 <hlee4786@usc.edu>
    Date:   Mon Mar 23 21:33:28 2026 -0700
    
        feat: updated UI + product display
    
    commit a6d62a6b87bad203d85d4959d7563e2e48758110
    Author: hlee4786 <hlee4786@usc.edu>
    Date:   Mon Mar 9 22:20:36 2026 -0700
    
        working Firestore to save a users' products
    
    commit 16b7b17811032c9fc5bf58a20dc4724304ca726b
    Merge: 772ef65a b5d6a50b
    Author: hlee4786 <hlee4786@usc.edu>
    Date:   Mon Feb 9 22:15:17 2026 -0800
    
        Merge branch 'hannah-app' of https://github.com/uscmakers/FIFO into hannah-app
    
    commit 772ef65a6d0281ccdc64003ad0fde5813e4fede1
    Author: hlee4786 <hlee4786@usc.edu>
    Date:   Mon Feb 9 22:12:55 2026 -0800
    
        firebase compiling

--- FIFO-App/src/firebase/config.ts
+++ FIFO-App/src/firebase/config.ts
@@ -1,12 +1,13 @@
 import { initializeApp } from "firebase/app";
 
-export const firebaseConfig = {
-  apiKey: /* insert API key from firestore */,
-  authDomain: /* insert authDomain from firestore */,
-  projectId: /* insert projectId from firestore */,
-  storageBucket: /* insert storageBucket from firestore */,
-  messagingSenderId: /* insert messagingSenderId from firestore */,
-  appId: /* insert appId from firestore */,
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
